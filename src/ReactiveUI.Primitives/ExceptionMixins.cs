14f53bf13fb8b240d500738eaa518abd5937e54e
Chris Pulman <chris.pulman@yahoo.com>
2026-05-25 14:18:58 +0100

    Add ReactiveUI.Primitives project and tooling
    Introduce the ReactiveUI.Primitives library and supporting infrastructure. Adds core primitives and helpers (RxVoid, Handle<T...>, LinqMixins, ExceptionMixins), two Roslyn generator projects (R3Bridge and SystemReactiveBridge) configured as IsRoslynComponent and referencing Microsoft.CodeAnalysis.CSharp, and wires the generators into the main csproj as packaged analyzers. Also adds a solution file, a benchmarks project, StyleCop settings, and a testconfig for CI/code-coverage.

---
+++ src/ReactiveUI.Primitives/ExceptionMixins.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2019-2023 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace ReactiveUI.Primitives;
+
+internal static class ExceptionMixins
+{
+    public static void Throw(this Exception exception)
+    {
+#if NET472 || NETSTANDARD2_0
+        System.Runtime.ExceptionServices.ExceptionDispatchInfo.Capture(exception).Throw();
+#endif
+        throw exception;
+    }
+}
---
+++ src/ReactiveUI.Primitives/Handle.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2019-2023 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using ReactiveUI.Primitives.Signals;
+
+namespace ReactiveUI.Primitives;
+
+internal static class Handle
+{
+    public static readonly Action Nop = () => { };
+    public static readonly Action<Exception> Throw = ex => ex.Throw();
+
+    public static IObservable<TSource> CatchIgnore<TSource>(Exception ex) =>
+        Signal.Empty<TSource>();
+}
